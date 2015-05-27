583935fa50146144378830c7229f4c28ad3dfeff
Fishouk <fishouk@yandex.ru>
2015-05-27 17:05:09 +0400

    v0.3

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 function htmlBodyClassToggleNight(){	
-	var htmlElementBody = document.getElementsByTagName("BODY")[0];
+	var htmlElementBody = document.getElementsByTagName("BODY")[0];	
 	htmlElementBody.classList.toggle("night");	
+	var htmlElementBodyClass = htmlElementBody.getAttribute("class");
 	if (typeof(Storage) != "undefined") {
     // Store
-    	localStorage.setItem("lastHtmlBodyClass", "htmlElementBodyClass");
+    	localStorage.setItem("lastHtmlBodyClass", htmlElementBodyClass);
 	} else {
     	alert("Извините, Ваш браузер не поддерживает Web Storage!");
 	}
 }
 function storageHtmlBodyClass(){
-	if (typeof(Storage) != "undefined") {
 		var htmlElementBody = document.getElementsByTagName("BODY")[0];
-	  	var htmlElementBodyClass = htmlElementBody.getAttribute("class");
-	  	if (htmlElementBodyClass === "night"){
 	  	var lastHtmlBodyClass = localStorage.getItem("lastHtmlBodyClass");
-	  	htmlElementBody.setAttribute("class", "lastHtmlBodyClass");
+	   	if (lastHtmlBodyClass === "day night"){	  	
+	  		htmlElementBody.setAttribute("class", lastHtmlBodyClass);
 	  	}
-  	} else {
-    	alert("Извините, Ваш браузер не поддерживает Web Storage!");
-	}
+  	
 }
